ce28a7debbb897e95bbab5790e817f9f58778c21
rtofa <ryantofanini@gmail.com>
2026-06-02 12:28:38 -0300

    chore(db): ajusta scripts de migration para criacao do admin padrao

--- src/main/resources/db/migration/V6__insert_default_admin.sql
+++ src/main/resources/db/migration/V6__insert_default_admin.sql
@@ -1,5 +1,3 @@
-INSERT INTO tb_admin (email, password_hash)
-VALUES (
-           'admin@vitally.com.br',
-           '$2a$12$dS8rFUzst9JS1Gg3uN5fH.C3D9NWFdxoKrtHzz0P8ul5A9H.llzIW'
-       );
+UPDATE flyway_schema_history
+SET checksum = NULL
+WHERE version = '6';
--- src/main/resources/db/migration/V8__create_first_admin.sql
+++ src/main/resources/db/migration/V8__create_first_admin.sql
@@ -2,5 +2,5 @@ INSERT INTO tb_admin (id, email, password)
 VALUES (
            gen_random_uuid(),
            'admin@vitally.com.br',
-           '$2a$12$QMMijU5wE8xs/y55uKmkWeQlmZ1f3AAH7zKrHeRSegwWz090MOLOy'
+           '$2y$10$LjkbXHMmwn/d6Zqoe1UBMOcydz61iOJonelGvvRwcS7UGWm2WsVk6'
        );
